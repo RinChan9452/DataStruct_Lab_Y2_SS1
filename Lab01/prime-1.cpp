8dfc8e2a08aec93f6efc273a9f42870dc9875776
RinChan <nattapol.taw@ku.th>
2025-07-16 20:04:24 +0700

    changed code in function checkPrime in prime-1.cpp

--- Lab01/prime-1.cpp
+++ Lab01/prime-1.cpp
@@ -1,32 +1,20 @@
 #include <iostream>
-#include <string>
 #include <stdio.h>
 using namespace std;
 
 void checkPrime(long long n) {
     
-    long long possition_count = 0;
-    
+    int possition_count = 1;
+    int count = 0;
+
     if (n < 2) {
         cout << "Unrelated";
         return;
     }
     
-    for (long long i = 2; i * i <= n; i++) {
-        if (n % i == 0){
-            cout << "Unrelated"; 
-            return;
-        }
+    for (long long a = 2; a <= n; a++){
+        possition_count++;
     }
-
-    for (long long i = 2; i <= n; i++) {
-        if (i == n) {
-            return count; 
-        }  
-    }
-
-    possition_count++; 
-    cout << possition_count;
     
 }
 
